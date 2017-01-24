f50da743f76a12d9423eee5087f00a274320c07d
Pauline122112 <pauline.design007@gmail.com>
2017-01-25 11:34:45 +1300

    aded for loop

--- minesweeper.js
+++ minesweeper.js
@@ -19,6 +19,9 @@ var board = {
 
 
 function startGame () {
+  for(i = 0; i < board.cells.length; i++){
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -43,4 +46,14 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+var surroundingCells =
+  lib.getSurroundingCells(cell.row, cell.col);
+  var count = 0;
+  for(var i = 0; i < surroundingCells.length; i++){
+    if(surroundingCells[i].isMine === true)
+    {
+      count += 1000;
+    }
+  }
+  return count;
 }
