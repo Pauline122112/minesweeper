1cc70b4e8f453f4af11e64b78908a74a8375af37
Pauline122112 <pauline.design007@gmail.com>
2017-01-29 22:14:29 +1300

    added win function

--- minesweeper.js
+++ minesweeper.js
@@ -42,11 +42,15 @@ function startGame () {
 // 2. Are all of the mines marked?
 function checkForWin () {
   for(i = 0; i < board.cells.length;i++){
+if ((board.cells[i].isMine && board.cells[i].isMarked) || (!board.cells[i].isMine && !board.cells[i].hidden)) {
 
+} else{
+  return;
+}
   }
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+lib.displayMessage('You win!')
 }
 
 // Define this function to count the number of mines around the cell
